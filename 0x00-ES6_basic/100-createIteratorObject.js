7b752d5c7bdb2ce0549284266d6123b4016ffc4d
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2023-07-26 22:12:21 +0100

    latest

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,13 @@
+export default function createIteratorObject(reportObject) {
+    const departments = Object.values(reportObject.allEmployees);
+  
+    return {
+      [Symbol.iterator]: function* () {
+        for (const department of departments) {
+          for (const employee of department) {
+            yield employee;
+          }
+        }
+      },
+    };
+  }
