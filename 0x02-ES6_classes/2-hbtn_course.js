a2561d4ad5de06322bdc3931f09abd91a41f573c
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2023-07-27 21:28:05 +0100

    update on task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students){
-    if (typeof name != String) {
+    if (typeof name != 'string') {
         throw new TypeError('Name must be a string');
-    } else if (typeof length != Number) {
+    } else if (typeof length != 'number') {
         throw new TypeError('Length must be a number');
     } else if (!Array.isArray(students)) {
         throw new TypeError('Students must be an array of strings');
