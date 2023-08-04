60ce0732e270cd435adb5f5fd148bad53234bf7b
Tayo Akinrinsoye <tayo.akinrinsoye@hotmail.com>
2023-08-04 14:12:54 +0100

    Task 4

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,5 @@
 export function taskFirst() {
-  const task = 'I prefer const when I can.'; // Using const to instantiate the variable
+  const task = 'I prefer const when I can.';
   return task;
 }
 
@@ -8,7 +8,7 @@ export function getLast() {
 }
 
 export function taskNext() {
-  let combination = 'But sometimes let'; // Using let to instantiate the variable
+  let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
