4a2c4dbb283dae664ef4f5f6cc0196acf0562295
Tayo Akinrinsoye <tayo.akinrinsoye@hotmail.com>
2023-08-04 10:28:23 +0100

    Task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
+/* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
-  let task = false; // Use let instead of var
-  let task2 = true; // Use let instead of var
+  const task = false; // Use let instead of var
+  const task2 = true; // Use let instead of var
 
   if (trueOrFalse) {
-    let task = true; // Use let instead of var (This is a different variable than the one declared outside the block)
-    let task2 = false; // Use let instead of var (This is a different variable than the one declared outside the block)
+    const task = true; // Use let instead of var
+    const task2 = false; // Use let instead of var
   }
-
   return [task, task2];
 }
