d859862b0185ba732b557c5356a261e88d290e9c
Tayo Akinrinsoye <tayo.akinrinsoye@hotmail.com>
2023-08-04 15:11:00 +0100

    Task 7

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,6 +1,4 @@
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = { income, gdp, capita }; // Using shorthand property notation
-  
+  const budget = { income, gdp, capita };
     return budget;
-  }
-  
+}
