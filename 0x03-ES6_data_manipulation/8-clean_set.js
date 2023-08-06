8aac9145f54ba751defcada983852e294102023a
Tayo Akinrinsoye <tayo.akinrinsoye@hotmail.com>
2023-08-06 07:00:21 +0100

    Task6,7 and 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+export default function cleanSet(set, startString) {
+  const pts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const val of set.values()) {
+    if (typeof val === 'string' && value.startsWith(startString)) {
+      const valSubStr = val.substring(startString.length);
+
+      if (valSubStr && valSubStr !== val) {
+        pts.push(valSubStr);
+      }
+    }
+  }
+  return pts.join('-');
+}
